bc943db37ff1cc4226116d331a74c73006a34c9f
Martin Fogliacco <fogliaccomartin3@gmail.com>
2022-11-19 19:47:06 -0300

    sanity backend integration + about dynamic section added

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -2,33 +2,23 @@ import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 
 import { images } from '../../constants'
+import { urlFor, client } from '../../client'
 
 import s from '../../styles/components/About/_about.module.scss'
 
-const abouts = [
-  {
-    title: 'Front-End Development', 
-    description: 'I am a good and passionate Front-End developer. That is the most exciting part of programming.',
-    imgUrl:images.about01
-  },
-  {
-    title: 'Back-End Development', 
-    description: 'I have knowledge about Back-End Development. Is not my strength, but I am working on it.',
-    imgUrl: images.about02
-  },
-  {
-    title: 'Server Development', 
-    description: 'I am  also able to develop servers with SQL technology. Currently, i am learning NoSQL to improve my skills.',
-    imgUrl: images.about03
-  },
-  {
-    title: 'UX/UI Oriented', 
-    description: 'I ever try to make webpages thinking in the user experience, and with an incredible user interface.',
-    imgUrl:images.about04
-  },
-]
-
 export default function About() {
+
+  const [abouts, setAbouts] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query)
+    .then((data) => setAbouts(data))
+
+  }, [])
+  
+
   return (
     <>
     <div className={s.app__aboutContainer} id='about'>
@@ -38,7 +28,7 @@ export default function About() {
     <div className={s.app__profiles}>
       {abouts.map((item, index) => {
         return <motion.div whileInView={{ opacity: 1 }} whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween'}} className={s.app__profileItem} key={item.title + index}>
-          <img src={item.imgUrl} alt={item.title} />
+          <img src={urlFor(item.imgUrl)} alt={item.title} />
           <h2 className={s.boldText} style={{marginTop: 20}}> {item.title} </h2>
           <h3 className={s.pText} style={{marginTop: 10}}> {item.description} </h3>
         </motion.div>
