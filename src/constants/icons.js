189861a10ee29c68bc1d9adc01c243975d455b84
Martin Fogliacco <fogliaccomartin3@gmail.com>
2024-03-07 03:11:16 -0300

    remove: unused icons

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -1,28 +1,25 @@
-import Spain from '../assets/espana.png';
-import Argentina from '../assets/argentina.png';
-import UnitedKingdom from '../assets/reino-unido.png';
-import Laptop from '../assets/Laptop-transparent.png';
-import PersonalLogo from '../assets/PersonalLogo.svg';
+import Spain from "../assets/espana.png";
+import Argentina from "../assets/argentina.png";
+import UnitedKingdom from "../assets/reino-unido.png";
+import Laptop from "../assets/Laptop-transparent.png";
+import PersonalLogo from "../assets/PersonalLogo.svg";
 
-import HTML5 from '../assets/techs-svg/html5.svg';
-import CSS3 from '../assets/techs-svg/css3.svg';
-import SASS from '../assets/techs-svg/sass.svg';
-import Tailwind from '../assets/techs-svg/tailwind-css.svg';
-import JavaScript from '../assets/techs-svg/javascript.svg';
-import React from '../assets/techs-svg/react.svg';
-import ReactNative from '../assets/techs-svg/react-native.svg';
-import Redux from '../assets/techs-svg/redux.svg';
-import Next from '../assets/techs-svg/next-js.svg';
-import Node from '../assets/techs-svg/nodejs-icon.svg';
-import Express from '../assets/techs-svg/express.svg';
-import PostgreSQL from '../assets/techs-svg/postgresql.svg';
-import MongoDB from '../assets/techs-svg/mongodb-icon.svg';
-import Figma from '../assets/techs-svg/figma.svg';
-import SocketIO from '../assets/techs-svg/socket-io.svg';
-import Spline from '../assets/techs-svg/spline.png';
-import Sanity from '../assets/techs-svg/sanity.svg';
-import Gsap from '../assets/techs-svg/gsap.svg';
-import FramerMotion from '../assets/techs-svg/framer-motion.svg';
+import HTML5 from "../assets/techs-svg/html5.svg";
+import CSS3 from "../assets/techs-svg/css3.svg";
+import SASS from "../assets/techs-svg/sass.svg";
+import Tailwind from "../assets/techs-svg/tailwind-css.svg";
+import JavaScript from "../assets/techs-svg/javascript.svg";
+import React from "../assets/techs-svg/react.svg";
+import Redux from "../assets/techs-svg/redux.svg";
+import Node from "../assets/techs-svg/nodejs-icon.svg";
+import Express from "../assets/techs-svg/express.svg";
+import PostgreSQL from "../assets/techs-svg/postgresql.svg";
+import MongoDB from "../assets/techs-svg/mongodb-icon.svg";
+import Figma from "../assets/techs-svg/figma.svg";
+import Spline from "../assets/techs-svg/spline.png";
+import Sanity from "../assets/techs-svg/sanity.svg";
+import Gsap from "../assets/techs-svg/gsap.svg";
+import FramerMotion from "../assets/techs-svg/framer-motion.svg";
 
 import { RiArrowDownSFill as ArrowDown } from "react-icons/ri";
 
@@ -39,17 +36,14 @@ export {
     Tailwind,
     JavaScript,
     React,
-    ReactNative,
     Redux,
-    Next,
     Node,
     Express,
     PostgreSQL,
     MongoDB,
-    SocketIO,
     Figma,
     Spline,
     Sanity,
     Gsap,
-    FramerMotion
-}
+    FramerMotion,
+};
